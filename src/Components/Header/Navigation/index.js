f9c39f296249e0c9e0d0578f14b89c827991a398
SHAFFAN12 <muhammadshaffanedu@gmail.com>
2024-06-12 13:25:35 +0500

    ShopShhere

---
+++ src/Components/Header/Navigation/index.js
@@ -0,0 +1,18 @@
+
+const Navigation = () => {
+    return(
+        <nav>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-sm-3 navPart1'>
+                        <Button className="CategoryTab">
+                            <span className="text">All categories</span>
+                        </Button>
+                    </div>
+                <div className='col-sm-9 navPart2'></div>
+            </div>
+            </div>
+        </nav>
+    )
+};
+export default Navigation;
